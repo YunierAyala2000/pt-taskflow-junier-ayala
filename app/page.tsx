130b66b9a4c544c8da4f0daee692bf2a425814f1
Yunier AP <ayalaperezyunier@hotmail.com>
2026-03-05 18:24:04 -0500

    ajustes de estilos alineacion de botones de nueva tarea y cambio de tema y ajustes de diseño

--- app/page.tsx
+++ app/page.tsx
@@ -78,16 +78,10 @@ export default function Home() {
           <div className="flex flex-col lg:grid lg:grid-cols-3 gap-6">
             <div className="lg:col-span-2 order-2 lg:order-1">
               <div className="bg-white dark:bg-gray-900 rounded-2xl shadow-sm border border-gray-200 dark:border-gray-800 p-4 md:p-6">
-                <div className="flex flex-col sm:flex-row sm:items-center sm:justify-between mb-4 md:mb-6 gap-2">
+                <div className="flex items-center justify-between mb-4 md:mb-6 gap-2">
                   <h2 className="text-lg md:text-xl font-semibold text-gray-900 dark:text-gray-100">
                     Mis Tareas
                   </h2>
-                  <TaskFilterTabs
-                    filter={filter}
-                    onChange={setFilter}
-                    counts={countsFiltered}
-                  />
-
                   <div className="flex items-center gap-2">
                     <ThemeToggle />
                     <Button
@@ -99,6 +93,13 @@ export default function Home() {
                       <PlusCircle className="size-4" />
                     </Button>
                   </div>
+                </div>
+                <div className="mb-2 md:mb-2">
+                  <TaskFilterTabs
+                    filter={filter}
+                    onChange={setFilter}
+                    counts={countsFiltered}
+                  />
                 </div>
 
                 <TaskItem
