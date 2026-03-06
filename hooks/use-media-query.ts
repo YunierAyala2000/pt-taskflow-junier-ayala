eca4557bdd36d703fb916a4a2bbd9bbd19283356
Yunier AP <ayalaperezyunier@hotmail.com>
2026-03-05 19:42:10 -0500

    Configuraciones para que el build funcione el build

--- hooks/use-media-query.ts
+++ hooks/use-media-query.ts
@@ -60,7 +60,7 @@ export function useIsTouchDevice(): boolean {
   const [isTouch, setIsTouch] = useState(false);
 
   useEffect(() => {
-    setIsTouch("ontouchstart" in window || navigator.maxTouchPoints > 0 || navigator.msMaxTouchPoints > 0);
+    setIsTouch("ontouchstart" in window || navigator.maxTouchPoints > 0);
   }, []);
 
   return isTouch;
